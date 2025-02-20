45749f155dc652c7d67a7ddcedda279597cddbda
MarceloNaviaR <marcelo.navia@gmail.com>
2025-02-20 13:31:21 -0400

    Funciones multiples

--- src/NumeroRomano.js
+++ src/NumeroRomano.js
@@ -3,6 +3,22 @@ function convertirANumerosRomanos(numero) {
       return 'I';
   } else if (numero === 2) {
       return 'II';
+  } else if (numero === 3) {
+      return 'III';
+  } else if (numero === 4) {
+      return 'IV';
+  } else if (numero === 5) {
+      return 'V';
+  } else if (numero === 6) {
+      return 'VI';
+  } else if (numero === 7) {
+      return 'VII';
+  } else if (numero === 8) {
+      return 'VIII';
+  } else if (numero === 9) {
+      return 'IX';
+  } else if (numero === 10) {
+      return 'X';
   }
   return '';
 }
--- src/NumeroRomano.spec.js
+++ src/NumeroRomano.spec.js
@@ -1,3 +1,4 @@
+// NumeroRomano.spec.js
 import convertirANumerosRomanos from './NumeroRomano';
 
 describe('Conversión a Números Romanos', () => {
@@ -8,6 +9,36 @@ describe('Conversión a Números Romanos', () => {
     test('Convierte 2 a II', () => {
         expect(convertirANumerosRomanos(2)).toBe('II');
     });
+
+    test('Convierte 3 a III', () => {
+        expect(convertirANumerosRomanos(3)).toBe('III');
+    });
+
+    test('Convierte 4 a IV', () => {
+        expect(convertirANumerosRomanos(4)).toBe('IV');
+    });
+
+    test('Convierte 5 a V', () => {
+        expect(convertirANumerosRomanos(5)).toBe('V');
+    });
+
+    test('Convierte 6 a VI', () => {
+        expect(convertirANumerosRomanos(6)).toBe('VI');
+    });
+
+    test('Convierte 7 a VII', () => {
+        expect(convertirANumerosRomanos(7)).toBe('VII');
+    });
+
+    test('Convierte 8 a VIII', () => {
+        expect(convertirANumerosRomanos(8)).toBe('VIII');
+    });
+
+    test('Convierte 9 a IX', () => {
+        expect(convertirANumerosRomanos(9)).toBe('IX');
+    });
+
+    test('Convierte 10 a X', () => {
+        expect(convertirANumerosRomanos(10)).toBe('X');
+    });
 });
-
-
